b4cdc46123d121f54705544486bdd36786d48543
Staffan Olsson <staffan@repos.se>
2018-04-15 10:08:46 +0200

    Uses the syntax that package-lock.json gets for local paths
    Suggests a change to https://github.com/Yolean/build-contract/pull/30

--- bin/pre-docker-build.sh
+++ bin/pre-docker-build.sh
@@ -3,17 +3,18 @@
 
 # https://github.com/Yolean/build-contract/pull/30/files#diff-9a789ca1e0c5686f221eaec3bf9dcda7R61
 if [ -z ${MONOREPO_DEPS+x} ]; then
-  MONOREPO_DEPS=$(grep '"../' package.json | awk -F '"' '{ print $4 }')
+  MONOREPO_DEPS=$(grep '"file:../' package.json | awk -F '"' '{ print $4 }')
 fi
 if [ ! -z "$MONOREPO_DEPS" ]; then
   mkdir -p npm-monorepo
   cp package.json package.json.monorepo-backup
-  for DEP in $MONOREPO_DEPS; do
+  for DEPFILE in $MONOREPO_DEPS; do
+    DEP=$(echo $DEPFILE | awk -F':' '{ print $2 }')
     pushd $DEP
     TARBALL=$(npm pack | tail -n 1)
     popd
     cp -v $DEP/$TARBALL npm-monorepo/
-    sed -i "s|$DEP|file://./npm-monorepo/$TARBALL|" package.json
+    sed -i "s|$DEPFILE|file:./npm-monorepo/$TARBALL|" package.json
   done
   echo "  --- monorepo compatibility ---  "
   git diff package.json
