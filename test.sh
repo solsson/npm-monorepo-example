0288f1329ee86702cd59c3ead61dd7b742340161
Staffan Olsson <staffan@repos.se>
2018-04-15 15:21:27 +0200

    Tracks the status of test scripts

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "\n---- tests expected to fail ----\n"
+set -x
+./test-npm-plain.sh
+./test-pnpm-lock.sh
+./test-pnpm-npm.sh
+echo "\n---- tests expected to pass ----\n"
+set -e
+./test-pnpm-lockfirst.sh
