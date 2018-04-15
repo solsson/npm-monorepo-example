a53870b5b41439568ab02816bfc82c6a66c20753
Staffan Olsson <staffan@repos.se>
2018-04-15 07:50:50 +0200

    Self check

--- module-b/index.js
+++ module-b/index.js
@@ -1,6 +1,13 @@
 let c = require('module-c');
 let Walker = require('walker');
 let w = Walker('./');
+let fs = require('fs');
+
+try {
+  fs.statSync('./node_modules/once');
+  console.error('Seeing a dev dependency from module-c');
+  process.exit(1);
+} catch(err) {}
 
 module.exports = function run(cb) {
   w.on('end', () => {
